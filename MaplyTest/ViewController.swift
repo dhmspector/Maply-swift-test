940092332e0e2632efa2348b3580f440d53f5cdc
David Spector <spector@zeitgeist.com>
2015-06-19 20:26:55 -0700

    Initial Commit

---
+++ MaplyTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaplyTest
+//
+//  Created by David HM Spector on 6/19/15.
+//  Copyright (c) 2015 Zeitgeist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
